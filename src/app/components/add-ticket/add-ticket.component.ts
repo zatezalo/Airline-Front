3e7ccc843ff059574b1eaeec7f4003e9d6184979
zatezalo <zatezalo123@gmail.com>
2020-12-17 20:53:18 +0100

    started add ticket form

--- src/app/components/add-ticket/add-ticket.component.ts
+++ src/app/components/add-ticket/add-ticket.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Company } from 'src/app/model/company.model';
+import { Ticket } from 'src/app/model/ticket.model';
+import { TicketService } from 'src/app/services/ticket/ticket.service';
 
 @Component({
   selector: 'app-add-ticket',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTicketComponent implements OnInit {
 
-  constructor() { }
+  public addTicketForm: FormGroup;
+
+  flights: Ticket[] = [];
+  companies: Company[] = [];
+  selectedF: number = 1;
+  
+  constructor(private ticketService: TicketService, private router: Router, private formBuilder: FormBuilder) {
+    this.addTicketForm = this.formBuilder.group({
+      flight: ['', Validators.required],
+      company: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  public get flight() {
+    return this.addTicketForm.get('flight');
+  }
+
+  public get company() {
+    return this.addTicketForm.get('company');
+  }
+
+  public submitForm(credentials) {
+    console.log(credentials);
+    /*this.ticketService.register(credentials).subscribe(data => {
+      console.log(data);
+      //this.router.navigate(['/']);
+    })*/
+  }
 }
